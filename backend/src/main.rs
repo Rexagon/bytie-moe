bcc11b8c4c95dac932834611ac88c49ca6f5312a
Rexagon <reide740@gmail.com>
2019-11-09 13:42:42 +0300

    Add backend stub

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files;
+use env_logger;
+use actix_web::{Responder, middleware::Logger, web, App, HttpServer};
+
+
+#[get("/")]
+fn index() -> impl Responder { "Hello World!" }
+
+
+fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .service(index)
+    })
+        .bind("0.0.0.0:8000")?
+        .run()
 }
